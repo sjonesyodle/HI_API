ea53b1f2f3ecac3bf21a17559f49e9a2508057ce
Steve Jones <sjones@yodle.com>
2013-07-14 22:33:39 -0400

    parses query results and generates html in colorbox

--- script.js
+++ script.js
@@ -43,6 +43,144 @@ this.mods[a]&&this.mods[a].init();return this}};return function(){var a=Object.c
                 return true;
             },
 
+            generateView : ( function () {
+	            var
+	            getStrPos = function ( str, substr ) {
+	                var pos = str.indexOf(substr), positions = [];
+	                
+	                while(pos > -1) {
+	                    positions.push(pos);
+	                    pos = str.indexOf(substr, pos+1);
+	                }
+	            
+	                return positions;
+	            },
+
+	            chars = function(str) {
+	              if (str == null) return [];
+	              return String(str).split('');
+	            },
+	            
+	            strSplice = function (start, length, word, str) {
+	               var arr = chars(str);
+	              arr.splice(start, length, word);
+	              return arr.join('');  
+	            },
+	            
+	            getVars = function ( template ) {
+	                var 
+	                varLocs = {},
+	                openVar  = "{{",
+	                closeVar = "}}";
+	            
+	                varLocs.startIdxs = getStrPos( template, openVar );
+	                varLocs.closeIdxs = getStrPos( template, closeVar );
+	                
+	                return varLocs;    
+	            },
+	            
+	            getObjVal = function (objStr, data) {
+	                var 
+	                objStr = objStr.split("."),
+	                nextLevel,
+	                i = 0, l = objStr.length,
+	                value = data;
+
+	                if (!value || $.isEmptyObject(value)) return;
+
+	                while ( i < l ) {
+	                    nextLevel = objStr[i];
+	                    value = value[ nextLevel ] ? value[ nextLevel ] : false;
+	                    if ( !value ) return;
+	                    i += 1;
+	                }
+	                
+	                return $.trim( value );
+	            },
+	            
+	            extractVarVals = function (valArr, data) {
+	                var
+	                i = 0,
+	                l = valArr.length;
+	                
+	                for ( ; i < l; i += 1) {
+	                    if ( !(valArr[i].value = getObjVal(valArr[i].objStr , data)) ) return false;
+	                }
+
+	                return valArr;
+	            },
+	            
+	            injectVarVals = function ( varList, template ) {
+	                var 
+	                i = 0,
+	                l = varList.length,
+	                start, stop, range, valLen, value, adjust = 0;
+	                
+	                for ( ; i < l ; i += 1 ) {
+	                    
+	                    value  = $.trim( varList[i].value );
+	                    valLen = varList[i].value.length;  
+	                    
+	                    start = varList[i].range[0];
+	                    stop  = varList[i].range[1];    
+	                    range = stop - start;
+	                    
+	                   
+	                    if (i > 0) {
+	                        start = varList[i].range[0] + adjust;
+	                        stop  = varList[i].range[1] + adjust;    
+	                        range = stop - start;
+	                    }
+	                        
+	                    template = strSplice( start, range, value , template  );
+	                    
+	                    adjust += valLen - range;
+	                }
+	                
+	                return template;
+	            },
+	            
+	            getVarValues = function ( template ) {
+	                var 
+	                varLocs   = getVars( template ),
+	                startLocs = varLocs.startIdxs,
+	                endLocs   = varLocs.closeIdxs,
+	                
+	                len_sl = startLocs.length,
+	                len_el = endLocs.length,
+	                i, pos1, pos2, range,
+	                
+	                varList = [], varItem;
+	            
+	                
+	                if (len_sl !== len_el) return false; // un-even var braces!
+	                
+	                i = 0;
+	                for ( ; i < len_sl; i += 1 ) {
+	                    pos1  = startLocs[i];
+	                    pos2  = endLocs[i];
+	                    
+	                    varList.push({
+	                        range  : [pos1, pos2 + 2],
+	                        objStr : $.trim( template.slice(pos1 + 2, pos2) ) 
+	                    });
+	                    
+	                }
+	                
+	                return varList;
+	            };
+	            
+	            return function ( template, data ) {
+	                var
+	                varList = getVarValues( template );
+
+	                if (!extractVarVals( varList, data )) return false;
+
+	                return injectVarVals( varList, template );
+	            };
+	            
+	        }()),
+
             utcNum: function () {
                 var uts = new Date().getTime().toString();
                 return uts.slice(uts.length - 9, uts.length - 1);
@@ -204,11 +342,68 @@ this.mods[a]&&this.mods[a].init();return this}};return function(){var a=Object.c
 		},
 
 
-		{// : Color-Box Modal for Selecting Locations :
+		{// : Color-Box Modal for displaying query results with user events:
+
+
+			nodes : {
+
+				displayLoc : ".userLocation"
+
+			},
+
+			tmpl : [
+				"<div class='HI_Location' data-franchisenum='{{franchiseNum}}' style='background:#666;padding:10px;margin: 10px auto'>",
+					"<span>{{franchiseName}}</span> <br />",
+					"<span>{{address1}}</span> <br />",
+					"<span>{{city}}, {{state}} {{zip}}</span> <br />",
+					"<span>{{phone}}</span> <br />",
+					"<span style='width:40px; height20px; background:green; cursor:pointer;' class='locSelect'>Select This Location</span>",
+				"</div>"
+			].join(""),
 
 			init : function () {
+				var that = this;
 
-			}
+				this.bind();
+
+				this._sub("location_results_updated", function( data ){
+
+					that.activeData = data;
+					that.activeHMTL	= "";
+
+					that.build();
+					that.show();
+				});
+			},
+
+			bind : function () {
+
+			},
+
+			build : function () {
+				var
+				data = this.activeData,
+				html = "", i, l, 
+				createView = this.cluster.util.generateView, tmpl = this.tmpl;
+
+				i = 0;
+				l = data.length;
+				for ( ; i < l; i += 1 ) {
+					html += createView( tmpl, data[i] );
+				}
+
+				this.activeHTML = html;
+			},
+
+			show : function () {
+				$.colorbox({
+					html : this.activeHTML 
+				})
+			},
+
+			selectLocation : function () {
+
+			}	
 
 		},
 
